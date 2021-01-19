b98f5df674444745a540b486fcf58ead4ff604b0
emilyhuangml <76086568+emilyhuangml@users.noreply.github.com>
2021-01-19 12:25:51 -0800

    Work Session 5: 1/19/20 11:20 - 12:10pm
    During this work session, I tried to finish the smallest multiple problems. However, it wasn't as easy as I thought. For some reason, my range kept being out of range even when I printed it individually, it gave the right answer. I wasn't very focused this time because I was tired but managed to add more to my code before and refined some areas. Additionally, since the worksheet homework took most of my time, I didn't have as much time to finish this problem.

--- #Smallest multiple.py
+++ #Smallest multiple.py
@@ -1,8 +1,14 @@
 #Smallest multiple
-import math
-type(math.inf)
+list20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+i = 1
+e = 1
+longlist = list(range(10000000))
+while i <= 20 and e <= 10000000:
+    if longlist[e] % list20[i] == 0:
+        i = i + 1
+        print (i)
+    else:
+        e = e + 1
 
-for i in range (1, 21):
-    for e in range (1, math.inf):
-        if e%i == 0:
-            print (e)
+
+
